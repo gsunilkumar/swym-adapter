eb25d6ba19b6de3672ee208474beadfbc8d908ad
Sunil Gaddam <sgaddam@ezesoft.com>
2022-03-16 18:32:35 +0530

    modularize merchant

--- src/adaptorImpl.py
+++ src/adaptorImpl.py
@@ -1,5 +1,5 @@
-import requests
 from swym.adaptor import Adaptor
+import requests
 
 class AdaptorImpl(Adaptor):
     def __init__(self, id):
@@ -28,25 +28,3 @@ class AdaptorImpl(Adaptor):
 
         print('Transformed data:',target)
         return target
-
-class Merchant(object):
-    def GetProduct(self):
-        print("Merchant:", self.Name, "GetProduct")
-        data = self.adaptorImpl.GetData()
-        return data
-
-    def __init__(self, id):
-        self.adaptorImpl = AdaptorImpl(id)
-        self.Name = "Merchant1"
-
-# adaptor = AdaptorImpl()
-# adaptor.GetMetadata()
-
-# source = {'a':12,'b':2}
-# target = {'x':1, 'y':2, 'z':4}
-# mapping = {'title': 'name'}
-# adaptor = AdaptorImpl()
-# adaptor.GetData()
-
-merchant1 = Merchant(1)
-merchant1.GetProduct()
